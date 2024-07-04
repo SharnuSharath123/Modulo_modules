f1da1d25b8d26000c525b1f5c0c7841615d4b99a
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-04 23:24:24 +0530

    Modules

---
+++ leaf_green.py
@@ -0,0 +1,26 @@
+# approach 1
+import leaf
+import green
+
+leaf.fly()
+leaf.color()
+leaf.behaviour()
+leaf.creative()
+
+green.fly()
+green.color()
+green.behaviour()
+green.creative()
+
+# approach 2
+from leaf import *
+fly()
+color()
+behaviour()
+creative()
+
+from green import *
+fly()
+color()
+behaviour()
+creative()
